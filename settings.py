a0b61bbdb93dd7ae93a4c872dd2dc9e1aef17c88
FrancoJame <mutebifrancis33@gmail.com>
2026-04-29 02:24:23 +0300

    Add files via upload

--- settings.py
+++ settings.py
@@ -22,14 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-(9-!3#mh4t#drr69hdc1=7sx4737u_n*y62xb!nd@g3s(c)lu!')
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
     'https://web-production-1000a.up.railway.app',
-    'https://*.railway.app',
     'https://*.up.railway.app'
 ]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 

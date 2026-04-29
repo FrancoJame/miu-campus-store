61491e14788dd70ed41e045543035dd1254f16dd
FrancoJame <mutebifrancis33@gmail.com>
2026-04-29 04:43:09 +0300

    Add files via upload

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from rest_framework.routers import DefaultRouter
-from . import views, api_views
+from . import views, api_views, forms
 
 router = DefaultRouter()
 router.register(r'products', api_views.ProductViewSet)
@@ -15,7 +15,10 @@ urlpatterns = [
     # path('products/<int:pk>/', views.product_detail, name='product_detail'), # To be implemented
     
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='core/login.html',
+        authentication_form=forms.LoginForm
+    ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     
     path('dashboard/', views.dashboard, name='dashboard'),

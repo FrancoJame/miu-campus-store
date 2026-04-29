b9445c167257a106d97d1269e187899ab00d94d7
FrancoJame <mutebifrancis33@gmail.com>
2026-04-29 03:46:43 +0300

    Add files via upload

--- miu_campus_store/urls.py
+++ miu_campus_store/urls.py
@@ -14,6 +14,9 @@ urlpatterns = [
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+from django.urls import re_path
+from django.views.static import serve
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]

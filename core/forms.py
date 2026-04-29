09ad7a2a65d2693b1c079c28b3efb11d47341195
FrancoJame <mutebifrancis33@gmail.com>
2026-04-29 04:42:33 +0300

    Add files via upload

--- core/forms.py
+++ core/forms.py
@@ -1,21 +1,39 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from .models import User, Product
 
 class SignupForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    password_confirm = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    username = forms.CharField(widget=forms.TextInput(attrs={'autocomplete': 'username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}))
+    password_confirm = forms.CharField(widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}), label="Confirm Password")
 
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'phone', 'address']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
 
     def clean(self):
         cleaned_data = super().clean()
         password = cleaned_data.get("password")
         password_confirm = cleaned_data.get("password_confirm")
 
-        if password != password_confirm:
-            raise forms.ValidationError("Passwords do not match")
+        if password and password_confirm:
+            if password != password_confirm:
+                self.add_error('password_confirm', "Passwords do not match")
+            
+            # Validate password strength using Django's built-in validators
+            try:
+                validate_password(password, self.instance)
+            except ValidationError as e:
+                self.add_error('password', e)
+                
         return cleaned_data
 
     def save(self, commit=True):
@@ -57,3 +75,23 @@ class CheckoutForm(forms.Form):
                 raise forms.ValidationError("Please fill in all card details for card payments.")
         
         return cleaned_data
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Username',
+            'autocomplete': 'username',
+            'autofocus': True
+        })
+    )
+    password = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+            'autocomplete': 'current-password'
+        })
+    )

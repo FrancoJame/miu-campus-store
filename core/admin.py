f03c1048f0c188efcd3a13ed68bad6a533983dae
FrancoJame <mutebifrancis33@gmail.com>
2026-04-29 05:05:20 +0300

    Add files via upload

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User, Category, Product, Order, OrderItem
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+
+# Admin Interface Customization (Classic Look)
+admin.site.site_header = "Site Administration"
+admin.site.site_title = "Site Administration"
+admin.site.index_title = "Site Administration"
+admin.site.enable_nav_sidebar = False  # Removes the sidebar to restore the classic layout
